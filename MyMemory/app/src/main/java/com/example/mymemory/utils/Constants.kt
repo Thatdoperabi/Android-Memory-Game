7cb669247ef7997fa062366002c34a08fcb18996
dprasun <dakotah.prasun@vizientinc.com>
2024-05-06 15:13:48 -0500

    memory game

---
+++ MyMemory/app/src/main/java/com/example/mymemory/utils/Constants.kt
@@ -0,0 +1,21 @@
+package com.example.mymemory.utils
+
+import com.example.mymemory.R
+
+
+
+val DEFAULT_ICONS = listOf(
+    R.drawable.ic_bloodelf,
+    R.drawable.ic_draenei,
+    R.drawable.ic_dwarf,
+    R.drawable.ic_gnomemale,
+    R.drawable.ic_goblin,
+    R.drawable.ic_nightelf,
+    R.drawable.ic_orc,
+    R.drawable.ic_panda,
+    R.drawable.ic_tauren,
+    R.drawable.ic_troll,
+    R.drawable.ic_undead,
+    R.drawable.ic_worgen
+
+)
